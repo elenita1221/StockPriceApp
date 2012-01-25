1b8c3a0023855678410f8f17897f93233c3412c9
Amit Parikh <amitparikh@gmail.com>
2012-01-24 23:44:58 -0500

    using render_template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import urllib
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,11 +12,16 @@ def __request(symbol, stat):
 def hello():
     return 'What\'s up, Lance?'
 
+@app.route('/stock/')
+def default_stock():
+    return 'Please enter a stock ticker symbol...'
+
 @app.route('/stock/<symbol>')
-def stock(symbol):
+def stock(symbol=None):
     # p = __request(symbol, 'l1c1va2xj1b4j4dyekjm3m4rr5p5p6s7').split(',')[0]
     p = __request(symbol, 'l1')
-    return '%s : %s' % (symbol, p)
+    # return '%s : %s' % (symbol, p)
+    return render_template('stock.html', s=symbol, p=p)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000
