202d746aef8af4c80295f3d2cef00e2b40618dc4
Amit Parikh <amitparikh@gmail.com>
2012-01-24 22:40:33 -0500

    Lance 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'What's up, Lance?'
+    return 'What\'s up, Lance?'
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000
